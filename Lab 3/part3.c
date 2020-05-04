ef5158af96e623c0197adc3827d77359fea052fb
mosesmgk <59423751+mosesmgk@users.noreply.github.com>
2020-05-04 14:37:58 -0700

    Create part3.c

---
+++ Lab 3/part3.c
@@ -0,0 +1,41 @@
+/*
+# Name: Moses Garcia
+# Date: 03/24/2020
+# Title: Lab3  Inter-process Communication – Pipes IPC
+# Description: To develop multi-process application programs
+To demonstrate the use of pipes as an inter-process communication (IPC) mechanism
+*/
+
+#include <stdio.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <sys/wait.h>
+#include <sys/types.h> 
+#include <sys/ipc.h>
+
+//main
+int main() { int fds[2]; 
+	pipe(fds);
+/*child 1 duplicates downstream into stdin */
+if (fork() == 0) 
+{
+	dup2(fds[0], 0); 
+	close(fds[1]); 
+	execlp("grep","grep","root", 0);
+}
+/*child 2 duplicates upstream into stdout */
+else if (fork() == 0) 
+{ 
+	dup2(fds[1], 1); 
+	close(fds[0]); 
+	execlp("cat", "cat","/etc/passwd",0);
+}
+else 
+{ /*parent closes both ends and wait for children*/ 
+	close(fds[0]);
+	close(fds[1]);
+	wait(0);
+	wait(0); 
+}
+return 0;
+ }
